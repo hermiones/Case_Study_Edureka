ad80d6cb95c51d17f997c73c3cdba0948fe47270
hermiones <sudiptadiya20@gmail.com>
2021-07-30 19:37:01 +0530

    Updated

--- Case Study 2.py
+++ Case Study 2.py
@@ -18,6 +18,13 @@ print(list(d.keys()))'''
 
 
 #4: Write a for loop that prints all elements of a list and their position in the list
-a = [4,7,3,2,5,9]
+'''a = [4,7,3,2,5,9]
 for i in a:
-    print("element:",i,"position",a.index(i))
+    print("element:",i,"position",a.index(i))'''
+
+
+#5:Please   write   a   program   which accepts  a   string   from   console   and   print   the characters that have even indexes
+string = input("Please enter your input")
+for i in string:
+    if (string.index(i))%2==0:
+        print(i)
