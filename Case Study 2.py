5d92ec1f7319cab6197424c0cdc53ccab3a1011e
hermiones <sudiptadiya20@gmail.com>
2021-07-30 19:13:19 +0530

    Updated

--- Case Study 2.py
+++ Case Study 2.py
@@ -1,12 +1,12 @@
 #1.What is the output of the following code?
-nums =set([1,1,2,3,3,3,4,4])
-print(len(nums))
+'''nums =set([1,1,2,3,3,3,4,4])
+print(len(nums))'''
 
 #ANS: 4
 
 #2.What will be the output?
-d ={"john":40, "peter":45}
-print(list(d.keys()))
+'''d ={"john":40, "peter":45}
+print(list(d.keys()))'''
 
 
 #Ans: ['john', 'peter']
