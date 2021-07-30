3208b6a0e8b3c51219e90b14700e3e8049aa65c9
hermiones <sudiptadiya20@gmail.com>
2021-07-30 19:12:04 +0530

    Updated

--- Case Study 2.py
+++ Case Study 2.py
@@ -1,3 +1,22 @@
+#1.What is the output of the following code?
+nums =set([1,1,2,3,3,3,4,4])
+print(len(nums))
+
+#ANS: 4
+
+#2.What will be the output?
+d ={"john":40, "peter":45}
+print(list(d.keys()))
+
+
+#Ans: ['john', 'peter']
+
+
+
+
+
+
+
 #4: Write a for loop that prints all elements of a list and their position in the list
 a = [4,7,3,2,5,9]
 for i in a:
